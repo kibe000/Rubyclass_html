8ec3140f688996da6bf4133f0bfd7d3161b908c1
kibe000 <joseruikibet@gmail.com>
2024-10-17 13:55:32 +0300

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+
+const colorButton=document.getElementById('colorButton);
+
+    colorButton.addEventListener('click', ()=>{
+        colorButton.style.backgroundColor=colorButton.style.background==='blue' ? '#ffffff':'blue';
+    });
